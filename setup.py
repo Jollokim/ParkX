15dae607689222e134982768e88ae29360f15981
Jollokim <joakimjensenj@gmail.com>
2020-10-08 14:20:06 +0200

    setup.py fila

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='prosjektnavn',
+    version='0.0.1',
+    packages=['prosjektnavn'],
+    include_package_data=True,
+    install_requires=[
+        "Kivy",
+        "kivy-deps.angle",
+        "kivy-deps.glew",
+        "kivy-deps.sdl2"
+    ],
+)
