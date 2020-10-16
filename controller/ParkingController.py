6eafe13d5b688461b9a156d5c1f9983765eea8da
Michal <mkowalski1999@gmail.com>
2020-10-16 10:43:09 +0200

    All tests in parkplace now pass

--- controller/ParkingController.py
+++ controller/ParkingController.py
@@ -2,6 +2,13 @@ class ParkingController:
     def __init__(self, gui, repository):
         self.gui = gui
         self.repository = repository
+        self.counter = 0
 
     def toString(self):
         return str(f"Gui: {self.gui} Repository: {self.repository}")
+
+    def getCounter(self):
+        return self.counter
+
+    def increaseCoutner(self):
+        self.counter += 1
--- test/when_creating_a_parkingplace.py
+++ test/when_creating_a_parkingplace.py
@@ -4,11 +4,13 @@ from controller.ParkingController import *
 
 class TestParkingPlace:
 
-    def __init__(self):
-        self.pController = ParkingController("gui", "repository")
+    def test_CreatesParkingControllerProperly(self):
+        pController = ParkingController("gui", "repository")
+        assert pController.toString() == "Gui: gui Repository: repository"
 
-    def testCreatesParkingControllerProperly(self):
-        assert self.pController.toString() == "Gui: gui Repository: repository"
+    def test_ThatCounterWorks(self):
+        pController = ParkingController("gui", "repository")
+        assert pController.getCounter() == 0
+        pController.increaseCoutner()
+        assert pController.getCounter() == 1
 
-    def testThatCounterWorks(self):
-        assert 0
