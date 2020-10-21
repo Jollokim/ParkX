91d2f4bfb7fc4bde97d5e056bb8f574211dc191a
ThomasJohannessen <54627448+ThomasJohannessen@users.noreply.github.com>
2020-10-21 17:06:10 +0200

    typo in test-name

--- parkx_kode/test/test_parkingplace_class.py
+++ parkx_kode/test/test_parkingplace_class.py
@@ -5,38 +5,35 @@ from model.parkingplace import *
 
 class Test_parkingplace_class:
 
-    def test_parameters_are_used_in_constructur_correct_id(self):
+    def test_parameters_are_used_in_constructor_correct_id(self):
         pplace = parkingplace("0001", "TestPlass", "Adresseveien1", "1234", "1", "50", "NULL", "Har lader")
         expected = "0001"
         actual = pplace.id
         assert actual == expected
         pytest.fail("An Error occurred: Id")
 
-
-    def test_parameters_are_used_in_constructur_correct_name(self):
+    def test_parameters_are_used_in_constructor_correct_name(self):
         pplace = parkingplace("0001", "TestPlass", "Adresseveien1", "1234", "1", "50", "NULL", "Har lader")
         expected = "TestPlass"
         actual = pplace.name
         assert actual == expected
         pytest.fail("An Error occurred: name")
 
-    def test_parameters_are_used_in_constructur_correct_address(self):
+    def test_parameters_are_used_in_constructor_correct_address(self):
         pplace = parkingplace("0001", "TestPlass", "Adresseveien1", "1234", "1", "50", "NULL", "Har lader")
         expected = "Adresseveien1"
         actual = pplace.address
         assert actual == expected
         pytest.fail("An Error occurred: address")
 
-
-    def test_parameters_are_used_in_constructur_correct_zipcode(self):
+    def test_parameters_are_used_in_constructor_correct_zipcode(self):
         pplace = parkingplace("0001", "TestPlass", "Adresseveien1", "1234", "1", "50", "NULL", "Har lader")
         expected = "1234"
         actual = pplace.zip_code
         assert actual == expected
         pytest.fail("An Error occurred: zipcode")
 
-
-    def test_parameters_are_used_in_constructur_correct_number_of_spaces(self):
+    def test_parameters_are_used_in_constructor_correct_number_of_spaces(self):
         pplace = parkingplace("0001", "TestPlass", "Adresseveien1", "1234", "1", "50", "NULL", "Har lader")
         expected = "1"
         actual = pplace.number_of_places
@@ -63,4 +60,5 @@ class Test_parkingplace_class:
         expected = "Har lader"
         actual = pplace.details
         assert actual == expected
-        pytest.fail("An Error occurred: details")
+        pytest.fail("An Error occurred:  details")
+
