1478b4c0d790a384a0beeb9f92393294129ae154
Jollokim <joakimjensenj@gmail.com>
2020-10-28 17:09:57 +0100

    import fix

--- parkx_kode/main.py
+++ parkx_kode/main.py
@@ -1,8 +1,8 @@
 from kivy.app import App
 from kivy.config import Config
 
-from controller.ParkingController import ParkingController
-from view.gui import Gui
+from parkx_kode.controller.ParkingController import ParkingController
+from parkx_kode.view.gui import Gui
 
 
 class MyApp(App):
