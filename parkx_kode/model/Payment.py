95d5c5ab4de7a042a222d21a179e281cfe28d97b
ThomasJohannessen <54627448+ThomasJohannessen@users.noreply.github.com>
2020-11-20 16:32:31 +0100

    flyttet betalings del til egen klasse

---
+++ parkx_kode/model/Payment.py
@@ -0,0 +1,31 @@
+class Payment:
+    def __init__(self, repository):
+        self.repository = repository
+        self.acceptedPaymentDetails = False
+
+    def add_new_payment(self, name, parkingstarted, parkingStopped, price_to_pay):
+        pay_dict = {
+            "name": name,
+            "parkingStarted": parkingstarted,
+            "parkingStopped": parkingStopped,
+            "price": price_to_pay
+        }
+        self.repository.add_payment(pay_dict)
+
+    def get_all_payments(self):
+        return self.repository.get_all_payments()
+
+    def pay_all_payments(self):
+        if self.acceptedPaymentDetails:
+            self.repository.remove_all_payments()
+            return True
+        else:
+            return False
+
+    def change_accepted_payment_details(self, buttonValueBool):
+        self.acceptedPaymentDetails = buttonValueBool
+
+    def check_accepted_payment_details(self):
+        return self.acceptedPaymentDetails
+
+
